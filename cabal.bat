bc499b20c5ef44859cd38662cca1ba7e51c84ad9
Arvin Chan <arvinchan24@gmail.com>
2014-01-20 12:45:13 +0800

    Update cabal.bat

--- cabal.bat
+++ cabal.bat
@@ -1,4 +1,4 @@
 c:
-del "C:\Users\ghian\Desktop\CABAL Online (PH)\GameGuard\*.erl"
+del "C:\Users\arvin\Desktop\CABAL Online (PH)\GameGuard\*.erl"
 
-rmdir /s /q "C:\Users\ghian\Desktop\CABAL Online (PH)\DATA\UserData"
+rmdir /s /q "C:\Users\arvin\Desktop\CABAL Online (PH)\DATA\UserData"
